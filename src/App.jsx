0aa8037bd4055f968de101b4c76a0c9b1f4880ee
L-Lifbom <141008398+L-Lifbom@users.noreply.github.com>
2024-03-11 18:50:30 +0100

    created map function to display alla jobs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,111 @@
 import './App.css'
 
+const data = [
+  {
+    id: 1,
+    logo: "./src/assets/logo-apple.png",
+    company: "Apple",
+    position: "Junior Frontend Developer",
+    role: "Frontend",
+    level: "Junior",
+    location: "USA/Remote",
+    contract: "Fulltime",
+  },
+  {
+    id: 1,
+    logo: "./src/assets/logo-apple.png",
+    company: "Apple",
+    position: "Junior Frontend Developer",
+    role: "Frontend",
+    level: "Junior",
+    location: "USA/Remote",
+    contract: "Fulltime",
+  },
+  {
+    id: 1,
+    logo: "./src/assets/logo-apple.png",
+    company: "Apple",
+    position: "Junior Frontend Developer",
+    role: "Frontend",
+    level: "Junior",
+    location: "USA/Remote",
+    contract: "Fulltime",
+  },
+  {
+    id: 1,
+    logo: "./src/assets/logo-apple.png",
+    company: "Apple",
+    position: "Junior Frontend Developer",
+    role: "Frontend",
+    level: "Junior",
+    location: "USA/Remote",
+    contract: "Fulltime",
+  },
+  {
+    id: 1,
+    logo: "./src/assets/logo-apple.png",
+    company: "Apple",
+    position: "Junior Frontend Developer",
+    role: "Frontend",
+    level: "Junior",
+    location: "USA/Remote",
+    contract: "Fulltime",
+  },
+  {
+    id: 1,
+    logo: "./src/assets/logo-apple.png",
+    company: "Apple",
+    position: "Junior Frontend Developer",
+    role: "Frontend",
+    level: "Junior",
+    location: "USA/Remote",
+    contract: "Fulltime",
+  },
+  {
+    id: 1,
+    logo: "./src/assets/logo-apple.png",
+    company: "Apple",
+    position: "Junior Frontend Developer",
+    role: "Frontend",
+    level: "Junior",
+    location: "USA/Remote",
+    contract: "Fulltime",
+  },
+]
+
 function App() {
 
   return (
     <>
       <Nav/>
-
       <main>
-
+        <table>
+          <thead>
+            <tr>
+              <th>Logo</th>
+              <th>Company</th>
+              <th>Position</th>
+              <th>Role</th>
+              <th>Level</th>
+              <th>Location</th>
+              <th>Contract</th>
+            </tr>
+          </thead>
+          {data.map((job) => {
+            return (
+              <Form
+                key={job.id}
+                logo={job.logo}
+                company={job.company}
+                position={job.position}
+                role={job.role}
+                level={job.level}
+                location={job.location}
+                contract={job.contract}
+              />
+            )
+          })}
+        </table>
       </main>
     </>
   )
@@ -15,13 +113,27 @@ function App() {
 
 function Nav() {
   return (
-    <nav>
-      <div>
-        <input type="text" />
-        <button className='nav-btn-1'>Filter</button>
-        <button className='nav-btn-2'>Clear</button>
-      </div>
-    </nav>
+    <div className='nav-bar'>
+      <input type="text" />
+      <button className='nav-btn-1'>Filter</button>
+      <button className='nav-btn-2'>Clear</button>
+    </div>
+  )
+}
+
+function Form({ logo, company, position, role, level, location, contract }) {
+  return (
+      <tbody className='data-template'>
+        <tr>
+          <td><img src={logo} alt="N/A"/></td>
+          <td>{company}</td>
+          <td>{position}</td>
+          <td>{role}</td>
+          <td>{level}</td>
+          <td>{location}</td>
+          <td>{contract}</td>
+        </tr>
+      </tbody>
   )
 }
 
