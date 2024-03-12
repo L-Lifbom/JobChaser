b8508a1073b1e56b5509d6184daecf1e58424693
L-Lifbom <141008398+L-Lifbom@users.noreply.github.com>
2024-03-12 11:55:36 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -74,7 +74,6 @@ const data = [
 ]
 
 function App() {
-
   return (
     <>
       <Nav/>
@@ -114,7 +113,7 @@ function App() {
 function Nav() {
   return (
     <div className='nav-bar'>
-      <input type="text" />
+      <input type="text" className='nav-input' placeholder='Search'/>
       <button className='nav-btn-1'>Filter</button>
       <button className='nav-btn-2'>Clear</button>
     </div>
