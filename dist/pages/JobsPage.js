c02c7cd0883437effcf72b200c215fb293b0931a
L-Lifbom <141008398+L-Lifbom@users.noreply.github.com>
2024-05-30 15:40:07 +0200

    dist fil fixad

--- dist/pages/JobsPage.js
+++ dist/pages/JobsPage.js
@@ -2,7 +2,7 @@ import { jsx as _jsx, jsxs as _jsxs, Fragment as _Fragment } from "react/jsx-run
 import { useState } from 'react';
 import Search from '../components/search';
 import Table from '../components/table';
-import FilterOperations from '../components/FilterOperations';
+import FilterOperations from '../components/FIlterOperations';
 function Main() {
     const [filter, setFilter] = useState('');
     return (_jsx(_Fragment, { children: _jsxs("div", { className: 'main-container', children: [_jsx(Search, { filter: filter, setFilter: setFilter }), _jsx(FilterOperations, {}), _jsx(Table, { filter: filter })] }) }));
